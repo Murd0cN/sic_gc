c14b9075af55ebd14ecb4843033024e6255b7f05
Pedro Tedim <1091234@isep.ipp.pt>
2018-12-17 18:20:17 +0000

    make it better

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace SiC_GC.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        /*public long CategoriaId { get; set; }
+        public long MaterialId { get; set; }
+        public long DimensaoId { get; set; }*/
+
+        public Product()
+        {
+
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SiC_GC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Models.SiCContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("SiCContext")));
+
+            services.AddCors();
+            //Ativar isto para DB local, não consegui usar SQL Server na minha máquina
+            /*services.AddDbContext<Models.SiCContext>
+                (options => options.UseInMemoryDatabase("SiCContext"));*/
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+            app.UseMvc();
+            app.UseMvc();
+        }
+    }
+}
